f140c6da050edab4d75635226dc4a79791e4fff7
jiabaolongbuaa <jiabaolong@sei.buaa.edu.cn>
2016-06-22 11:09:13 +0800

    Revert "init commit"
    This reverts commit 36bbb1266978314480dea790a03d1e531d34dfed.

--- src/com/app/server/da/IEntityQueryFactory.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-package com.app.server.da;
-
-public interface IEntityQueryFactory {
-
-	<E> IEntityQuery<E> createQuery(Class<E> clazz);
-}
--- src/com/app/server/util/OpenfireStatus.java
+++
@@ -1,5 +0,0 @@
-package com.app.server.util;
-
-public class OpenfireStatus {
-	public static final String USER_CREATED = "Created";
-}
